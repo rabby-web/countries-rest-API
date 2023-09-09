ee218b797dfc87370ff31e5d48ec9e3c4aa75157
rabby-web <rabby.webdeveloper@gmail.com>
2023-09-09 17:31:55 +0600

    set useEffect and useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
+  const [country, setCountry] = useState()
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => setCountry(data))
+  },[])
 
   return (
     <>
-      <h1>Hello</h1>
+      <div>
+        <h2 className='text-center font-bold text-3xl'>Countries {country.length}</h2>
+      </div>
     </>
   )
 }
