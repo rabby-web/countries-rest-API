90e26fca61cf44c04c07be1cd594e1041aaea458
rabby-web <rabby.webdeveloper@gmail.com>
2023-09-09 18:27:54 +0600

    capita show

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
 const Countries = ({countries}) => {
-    const {area,continents,flags} = countries;
+    const {continents,flags, capital,name} = countries;
     console.log(countries)
     return (
         <div>
           <div className="card w-full bg-base-100 shadow-xl">
             <figure><img className='h-52 w-full' src={flags.png} alt="Shoes" /></figure>
             <div className="card-body">
-              <h2 className="card-title">
+              <h2 className="card-title text-2xl">
                 {continents}
-                <div className="badge badge-secondary">NEW</div>
+                <div className="badge badge-secondary">{capital}</div>
               </h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <p>{name.common}</p>
               <div className="card-actions justify-end">
                 <div className="badge badge-outline">Fashion</div> 
                 <div className="badge badge-outline">Products</div>
